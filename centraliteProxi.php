11ca38d4b0faa7d65ea6203274b8079a0d1de17a
Phannarat Horng <horng.phannarat@courrier.uqam.ca>
2014-03-25 00:40:11 -0400

    test de dijkstra

--- centraliteProxi.php
+++ centraliteProxi.php
@@ -1,32 +1,45 @@
 <?php
+require_once 'dijkstra.php';
+function centraliteProxi($intervenantIdI, $nbNoeud) {
+	//echo $nbNoeud;
+	
+	// calcul de la centralite
+	$degree = ($nbNoeud-1)/sumDist($intervenantIdI);
 
- function centraliteProxi($intervenantIdI, $nbNoeud) {
-
-//echo $nbNoeud;
-
-// calcul de la centralite
-$degree = ($nbNoeud-1)/sumDist($intervenantIdI);
-
-return $degree;
+	return $degree;
 }
 
 function sumDist($intervenantIdI) {
-  $sum = 0;
-  
-  $query="SELECT *  from (((select distinct debutLienId as noeuds from liens ) 
-union (select distinct finLienId as noeuds from liens))as tableNoeud);";
- $queryResult = Mysql_Query($query);
+	$sum = 0;
+	$query="SELECT *  from (((select distinct debutLienId as noeuds from liens) 
+			union (select distinct finLienId as noeuds from liens))as tableNoeud);";
+	$queryResult = mysql_Query($query);
+	$reseau = creerGraphe();
  
- while($row = mysql_fetch_assoc($queryResult)) {        
-    	$intervenantIdJ = $row["noeuds"];      
-      if(strcmp($intervenantIdI,$intervenantIdJ)!=0) {
-         $sum = $sum + dist($intervenantIdI,$intervenantIdJ);
+	while($row = mysql_fetch_assoc($queryResult)) {        
+		$intervenantIdJ = $row["noeuds"];      
+		if(strcmp($intervenantIdI,$intervenantIdJ)!=0) {
+			list($chemin, $sumDist) = dijkstra($reseau, $intervenantIdI, $intervenantIdJ);
+			$sum += $sumDist;
       }
-  }
-  return $sum;
+	}
+  
+	return $sum;
 }
 
+function creerGraphe(){
+	$graph = array();
+	$query = "SELECT * FROM liens";
+	$queryResult = mysql_query($query);
 
+	while($row = mysql_fetch_assoc($queryResult)){
+		array_push($graph, array($row["debutLien"], $row["finLien"], $row["poids"]));
+	}
+
+	return $graph;
+}
+
+/*
 function dist($intervenantIdI,$intervenantIdJ) {
   $dist = 1; 
   //http://tel.archives-ouvertes.fr/docs/00/61/91/77/PDF/These_Nacim_Chikhi_v8.0.pdf
@@ -34,8 +47,5 @@ function dist($intervenantIdI,$intervenantIdJ) {
 return $dist;
     
 }
-
-
-
-
+*/
 ?>
---
+++ dijkstra.php
@@ -0,0 +1,54 @@
+<?php
+//http://rosettacode.org/wiki/Dijkstra's_algorithm
+function dijkstra($graph_array, $source, $target) {
+    $vertices = array();
+    $neighbours = array();
+	$sumDistance = 0;
+    foreach ($graph_array as $edge) {
+        array_push($vertices, $edge[0], $edge[1]);
+        $neighbours[$edge[0]][] = array("end" => $edge[1], "cost" => $edge[2]);
+    }
+    $vertices = array_unique($vertices);
+ 
+    foreach ($vertices as $vertex) {
+        $dist[$vertex] = INF;
+        $previous[$vertex] = NULL;
+    }
+ 
+    $dist[$source] = 0;
+    $Q = $vertices;
+    while (count($Q) > 0) {
+        $min = INF;
+        foreach ($Q as $vertex){
+            if ($dist[$vertex] < $min) {
+                $min = $dist[$vertex];
+                $u = $vertex;
+				$sumDistance = $min;
+            }
+        }
+ 
+        $Q = array_diff($Q, array($u));
+        if ($dist[$u] == INF or $u == $target) {
+            break;
+        }
+ 
+        if (isset($neighbours[$u])) {
+            foreach ($neighbours[$u] as $arr) {
+                $alt = $dist[$u] + $arr["cost"];
+                if ($alt < $dist[$arr["end"]]) {
+                    $dist[$arr["end"]] = $alt;
+                    $previous[$arr["end"]] = $u;
+                }
+            }
+        }
+    }
+    $path = array();
+    $u = $target;
+    while (isset($previous[$u])) {
+        array_unshift($path, $u);
+        $u = $previous[$u];
+    }
+    array_unshift($path, $u);
+    return array($path, $sumDistance);
+}
+?>
